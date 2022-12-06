08eb3abd944f35a2b1ed8caa232dfe415bc21918
AlfredoHJDLO <alfredo_HJDLO@yeah.net>
2022-12-06 11:12:53 -0600

    jdskjfklsdafjadsk;ljfdsjklf

---
+++ c++/Lenguaje/Lenguaje_compu.h
@@ -0,0 +1,38 @@
+#inlcude <iostream.h>
+using namespace std;
+
+class Lenguaje_Humano: public Lenguaje
+{
+  private:
+  string idioma;
+  public:
+  Lenguaje_Humano(string, string);
+  Lenguaje_Humano(string);
+  Lenguaje_Humano();
+  void setIdioma(string);
+  string getIdioma()const;
+};
+
+Lenguaje_Humano::Lenguaje_Humano(string simbolo, string lenguaje) : Lenguaje(simbolo)
+{
+  setIdioma(lenguaje);
+}
+
+Lenguaje_Humano::Lenguaje_Humano(string simbolo) : Lenguaje(simbolo)
+{
+  setIdioma("Español");
+}
+
+Lenguaje_Humano::Lenguaje_Humano():Lenguaje()
+{
+  setIdioma("Español");
+}
+
+void Lenguaje_Humano::setIdioma(string idioma)
+{
+  this->idioma = idioma;
+}
+string Lenguaje_Humano::getIdioma() const
+{
+  return idioma;
+}

82b29b25cb846771191a771b288fe3b7b6742f90
AlfredoHJDLO <alfredo_HJDLO@yeah.net>
2022-12-09 13:39:27 -0600

    terminado la clase persona v;v;;vv;

--- c++/Persona/Persona Py/Persona.py
+++ c++/Persona/Persona Py/Persona.py
@@ -31,4 +31,65 @@ class Estudiante(Persona):
     def getMatricula(self):
         return self.matricula
 
+    def setGRado(self, grado):
+        self.grado = grado
     
+    def getGrado(self):
+        return self.grado
+    
+    def setCarrera(self, carrera):
+        self.carrera = carrera
+
+    def getCarrera(self):
+        return self.carrera
+
+class Profesor(Persona):
+    def __init__(self, nombre, edad, sexo, materia, instituto, cubo):
+        super().__init__(nombre, edad, sexo)
+        self.setMateria(materia)
+        self.setIstituto(instituto)
+        self.setCubo(cubo)
+
+    def setMateria(self, mateira):
+        self.materia = mateira
+    
+    def getMateria(this):
+        return this.materia
+    
+    def setInstituto(this, instituto):
+        this.instituto = instituto
+    
+    def getInstituto(this):
+        return this.instituto
+    
+    def setCubo(this, cubo):
+        this.cubo = cubo
+
+    def getCubo(this):
+        return this.cubo
+
+class Administrativo(Persona):
+    def __init__(self, nombre, edad, sexo, departamento, funcion, sueldo):
+        super().__init__(nombre, edad, sexo)
+        self.setDepartamento(departamento)
+        self.setFuncion(funcion)
+        self.setSueldo(sueldo)
+
+    def setSueldo(this, sueldo):
+        this.sueldo = sueldo
+
+    def getSueldo(this):
+        return this.sueldo
+    
+    def setFuncion(this, funcion):
+        this.funcion = funcion
+    
+    def getFuncion(this):
+        return this.fuincion
+
+    def setDepartamento(this, departamento):
+        this.departamento = departamento
+    
+    def getDepartamento(this):
+        return this.departamento
+
